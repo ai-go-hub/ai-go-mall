7a255b892cb0a24389e52459446ca1054419e01b
yang <1094963513@qq.com>
2026-06-16 01:29:04 +0800

    feat: 路由加载后根据路由配置设置浏览器标题

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,6 +1,8 @@
+import { useTitle } from '@vueuse/core'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 import { createRouter, createWebHashHistory } from 'vue-router'
+import i18n from '/@/lang/index'
 import staticRoutes from '/@/router/static'
 import { loading } from '/@/utils/loading'
 
@@ -21,11 +23,16 @@ router.beforeEach(() => {
 })
 
 // 路由加载后
-router.afterEach(() => {
+router.afterEach((to) => {
     if (window.loading) {
         loading.hide()
     }
     NProgress.done()
+
+    // 设置浏览器标题
+    const titleKey = to?.meta?.title as string | undefined
+    const title = titleKey && i18n.global.te(titleKey) ? i18n.global.t(titleKey) : ''
+    useTitle().value = title ? `${title} - AI GO MALL` : 'AI GO MALL'
 })
 
 export default router
