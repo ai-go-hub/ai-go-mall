d96c79ad9c2122993bc5ddda421a95537fa68e0f
yang <1094963513@qq.com>
2026-06-20 02:30:39 +0800

    feat: 文件系统公共工具包增加 ReadDir 函数

--- pkg/filesystem/filesystem.go
+++ pkg/filesystem/filesystem.go
@@ -1,6 +1,9 @@
 package filesystem
 
-import "path/filepath"
+import (
+	"os"
+	"path/filepath"
+)
 
 // TrimExt 返回去除了路径和扩展名的文件名
 // path:文件路径或完整文件名
@@ -8,4 +11,19 @@ func TrimExt(path string) string {
 	name := filepath.Base(path)
 	ext := filepath.Ext(name)
 	return name[:len(name)-len(ext)]
+}
+
+// ReadDir 递归读取目录下所有文件，返回完整路径列表
+func ReadDir(dir string) ([]string, error) {
+	var paths []string
+	err := filepath.WalkDir(dir, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if !d.IsDir() {
+			paths = append(paths, path)
+		}
+		return nil
+	})
+	return paths, err
 }
